9da2858fba970cbad4fbb79841722e0d0ab59ee0
umakant <umakanth.shivabal@verticurl.com>
2021-08-30 11:15:36 +0530

    intital commit

---
+++ src/components/hero.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { GatsbyImage } from 'gatsby-plugin-image'
+
+import * as styles from './hero.module.css'
+
+const Hero = ({ image, title, content }) => (
+  <div className={styles.hero}>
+    {image && (
+      <GatsbyImage className={styles.image} alt={title} image={image} />
+    )}
+  </div>
+)
+
+export default Hero
